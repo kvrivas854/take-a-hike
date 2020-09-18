3bbb80e83fa5268fdae40e8de5c576168cee1f96
Sarah Shealy <sshealy124@gmail.com>
2020-09-17 21:24:04 -0400

    added more props

--- take-a-hike/src/components/Card/index.js
+++ take-a-hike/src/components/Card/index.js
@@ -15,7 +15,12 @@ function Card(props) {
 
             <div className="bg-green-700 px-6 py-4">
                 <div className="font-bold text-white text-xl mb-2">{props.trailName}</div>
+                <h4 className="text-white">{props.stars} stars</h4>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" style={{height="10px", width="10px"}} viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg> */}
                  <p className="text-white rounded bg-green-300 p-4 text-base">{props.summary}</p>
+                 <h3 className="text-white">{props.condition}</h3>
+                 <h3 className="text-white">{props.ascent}</h3>
+                 <h3 className="text-white">{props.length}</h3>
             </div>
 
             <div className="rounded bg-green-500 px-6 pt-4 pb-2">
