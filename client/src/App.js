8885b0dfa036c74f42bb2107ffe41f741b2e865f
Sarah Shealy <sshealy124@gmail.com>
2020-09-19 14:14:23 -0400

    initialized passport

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Navbar from './components/Navbar';
 import Header from './components/Header';
 // import {BrowserRouter as Router, Route} from "react-router-dom";
 import Search from './pages/Search'
+import Login from './pages/Login'
 import './App.css';
 
 console.log(process.env.REACT_APP_API_KEY);
 
-const App = () => (
+function App(){
+  // When the application starts, find out if a user is logged in
 
-  <div>
+ return( <div>
     <Header/>
-
-    <Search/>
-  </div>
+    <Login />
+    {/* {!loggedInUser && <Login setLoggedInUser={loggedInUser}/>}
+    {loggedInUser && <Search user={loggedInUser} />} */}
+  </div>)
     
-);
+};
+
+//21-MERN/01/05-stu route react router
 
 export default App;
