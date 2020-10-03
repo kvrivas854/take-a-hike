91c4ce536963bbefeb61dad4db79aff6576741d7
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-03 11:48:29 -0400

    search card css update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 // import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Contact from './components/Contact';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Search from './pages/Search'
 import Login from './pages/Login'
@@ -30,6 +31,8 @@ return (
    )}>
      <RegistrationForm />
    </Route>
+   <Route exact path="/contact">
+     <Contact/></Route>
    
    </Switch>
 
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -6,37 +6,33 @@ function Card(props) {
 console.log(props.index)
     return (
 
-    <div className="w-1/2 text-center">
-    <div className="box-border sm:box-content p-4 m-6 max-w-sm ">
-        
-
-
-            <div className="relative rounded open-sans bg-green-700 px-6 py-4">
+    <div className="flex">
+        <div className="box-border sm:box-content m-4">
+        <div className="bg-gray-100 px-6 pt-4 pb-2 focus:outline-none focus:shadow lobster font-bold text-xl">{props.trailName}</div>
+            <div className="relative open-sans bg-white px-6 py-4">
             
-                <div className="flex flex-col lobster font-bold text-white text-xl mb-2">{props.trailName}
-                <img className="flex-col p-2 h-32 w-32 rounded-full align-center sm:box-content" src={props.image} alt ="error: not found"/></div>
-                <h4 className="text-white">{props.stars} Stars</h4>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" style={{height="10px", width="10px"}} viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg> */}
-                 <p className="text-white rounded bg-green-300 p-4 text-base">{props.summary}</p>
-                 <h3 className="text-white">{props.condition}</h3>
-                 <h3 className="text-white">{props.ascent}</h3>
-                 <h3 className="text-white">{props.length}</h3>
-                 <button className="bg-green-200 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" data-id={props.index} onClick={props.handleClick}>
-  + Add to favorites
-</button>
+                <div className="flex">
+                    <img className="p-2 h-32 w-32 rounded-full sm:box-content" src={props.image} alt ="error: not found"/>
+                    <div className="flex flex-col">
+                        <h4 className="p-2">{props.stars} Stars</h4>
+                        <p className="rounded p-2 text-base">{props.summary}</p>
+                        <button className="w-max-content bg-green-500 hover:bg-green-400 text-white font-bold py-1 px-4 mt-4 border-b-2 hover:border-green-500 rounded" data-id={props.index} onClick={props.handleClick}>
+                            + Add to favorites
+                        </button>
+                    </div>
+                 </div>
+                 <h3 className="">{props.condition}</h3>
+                 <h3 className="">{props.ascent}</h3>
+                 <h3 className="">{props.length}</h3>
             </div>
-
-            <div className="rounded bg-green-500 px-6 pt-4 pb-2">
-                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-yellow-200 mr-2 mb-2">{props.loc}</span>
-                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-yellow-200 mr-2 mb-2">#{props.dif}</span>
-                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-yellow-200 mr-2 mb-2">#{props.type}</span>
+            <div className=" bg-gray-100 px-6 pt-4 pb-2 hover:bg-gray-500 focus:outline-none focus:shadow">
+                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-black-200 mr-2 mb-2">{props.loc}</span>
+                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-black-200 mr-2 mb-2">#{props.dif}</span>
+                <span className="inline-block bg-white-200 rounded-full px-3 py-1 text-sm font-semibold text-black-200 mr-2 mb-2">#{props.type}</span>
 
             </div>
-            
         </div>
-
-
-        </div>
+    </div>
     
         
     )
