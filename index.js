6ebc996aee4b5b026f685e7c98b93bae3e26860e
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-06 14:34:28 -0400

    jawsdb

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const passport = require("./config/passport");
 const mysql = require("mysql");
 const PORT = process.env.PORT || 8080;
+var mysql = require('mysql');
 
 
 // Define middleware here
@@ -17,12 +18,21 @@ app.use(express.json());
 // }
 
 // Add routes, both API and view
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-} 
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'BentleyBear8!',
+        database: 'takeahikedb'
+    })
+};
 
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -31,17 +41,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
+
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
 // require("./routes/api/index.js")(app);
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    user: 'root',
-    password: 'BentleyBear8!',
-    database: 'takeahikedb'
-  });
-};
 // Start the API server {force: true}
 db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
