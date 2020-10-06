c66fbc8efef9b660dbd10fcacaeaa69eece72744
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-06 14:59:25 -0400

    serve build

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: 'BentleyBear8!',
+        password: process.env.SQLPASSWORD,
         database: 'takeahikedb'
     })
 };
