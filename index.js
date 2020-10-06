fdfe73f2f792aa135f0fddaac75b68805391bded
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-06 13:58:40 -0400

    env

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // }
 
 // Add routes, both API and view
-if (process.env.NODE_ENV === "production"{
+if (process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
