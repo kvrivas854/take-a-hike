69b176d33f488adfe354aed2d0f78b21148a087c
Sarah Shealy <sshealy124@gmail.com>
2020-10-15 14:44:56 -0400

    changes

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const session = require("express-session");
-var db = require("./models");
-const routes = require("./routes");
-const app = express();
-const passport = require("./config/passport");
-const PORT = process.env.PORT || 8080;
+// const express = require("express");
+// const session = require("express-session");
+// var db = require("./models");
+// const routes = require("./routes");
+// const app = express();
+// const passport = require("./config/passport");
+// const PORT = process.env.PORT || 8080;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("take-a-hike/src"));
 // }
 // Add routes, both API and view
 
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+// );
 
-app.use(routes);
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use(routes);
 // require("./routes/api/index.js")(app);
 
 // Start the API server {force: true}
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
-  });
+// db.sequelize.sync().then(function() {
+//     app.listen(PORT, function() {
+//       console.log("App listening on PORT " + PORT);
+//     });
+//   });
